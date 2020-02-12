d5b6ba3de072f09e28a893283defba75494b421f
HomerIan <47555113+HomerIan@users.noreply.github.com>
2020-02-12 08:35:45 +0800

    2nd commit

--- guisystem.sql
+++ guisystem.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 10, 2020 at 02:04 AM
+-- Generation Time: Feb 12, 2020 at 01:32 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.2
 
@@ -25,21 +25,43 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `account`
+--
+
+CREATE TABLE `account` (
+  `accNo` varchar(5) NOT NULL,
+  `userName` varchar(20) DEFAULT NULL,
+  `passWord` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `account`
+--
+
+INSERT INTO `account` (`accNo`, `userName`, `passWord`) VALUES
+('ACC01', 'admin', 'admin');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `customer`
 --
 
 CREATE TABLE `customer` (
-  `custId` int(10) NOT NULL,
-  `firstName` varchar(25) DEFAULT NULL,
-  `lastName` varchar(25) DEFAULT NULL
+  `custNo` varchar(5) NOT NULL,
+  `custname` varchar(25) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `customer`
 --
 
-INSERT INTO `customer` (`custId`, `firstName`, `lastName`) VALUES
-(1, 'homer', 'reyes');
+INSERT INTO `customer` (`custNo`, `custname`) VALUES
+('C01', 'Homer Reyes'),
+('C02', 'Bon Tura'),
+('C03', 'Troy Gallardo'),
+('C04', 'Jared Gotgotao'),
+('C05', 'Mary Jane Lima');
 
 -- --------------------------------------------------------
 
@@ -48,11 +70,22 @@ INSERT INTO `customer` (`custId`, `firstName`, `lastName`) VALUES
 --
 
 CREATE TABLE `product` (
-  `prodCode` int(10) NOT NULL,
-  `prodName` varchar(25) DEFAULT NULL,
+  `prodCode` varchar(5) NOT NULL,
+  `description` varchar(20) DEFAULT NULL,
   `price` decimal(10,2) DEFAULT NULL,
   `weight` decimal(10,2) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`prodCode`, `description`, `price`, `weight`) VALUES
+('P01', 'Pencil', '10.00', '10.00'),
+('P02', 'Ballpen', '20.00', '10.00'),
+('P03', 'Notebook', '25.00', '20.00'),
+('P04', 'Calculator', '250.00', '35.00'),
+('P05', 'Ruler', '15.00', '5.00');
 
 -- --------------------------------------------------------
 
@@ -61,54 +94,86 @@ CREATE TABLE `product` (
 --
 
 CREATE TABLE `sales` (
-  `salesNum` int(10) NOT NULL,
-  `custId` int(10) NOT NULL,
-  `prodCode` int(10) NOT NULL
+  `transNo` varchar(5) NOT NULL,
+  `salesDate` date DEFAULT NULL,
+  `custNo` varchar(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `sales`
+--
+
+INSERT INTO `sales` (`transNo`, `salesDate`, `custNo`) VALUES
+('TR01', '2020-01-19', 'C01'),
+('TR02', '2020-02-01', 'C02'),
+('TR03', '2020-02-05', 'C03'),
+('TR04', '2020-01-25', 'C04'),
+('TR05', '2020-01-30', 'C05');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `salesdetail`
+--
+
+CREATE TABLE `salesdetail` (
+  `transNo` varchar(5) NOT NULL,
+  `prodCode` varchar(5) NOT NULL,
+  `quantity` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `salesdetail`
+--
+
+INSERT INTO `salesdetail` (`transNo`, `prodCode`, `quantity`) VALUES
+('TR01', 'P01', 1),
+('TR01', 'P02', 2),
+('TR01', 'P03', 1),
+('TR01', 'P04', 1),
+('TR01', 'P05', 1),
+('TR02', 'P01', 1),
+('TR02', 'P02', 1),
+('TR02', 'P03', 1),
+('TR02', 'P04', 1),
+('TR02', 'P05', 1),
+('TR03', 'P01', 2),
+('TR03', 'P02', 2),
+('TR03', 'P03', 1),
+('TR03', 'P04', 1),
+('TR03', 'P05', 2),
+('TR04', 'P01', 1),
+('TR04', 'P02', 2),
+('TR04', 'P03', 2),
+('TR04', 'P05', 1),
+('TR05', 'P01', 1),
+('TR05', 'P02', 1),
+('TR05', 'P03', 3),
+('TR05', 'P04', 2),
+('TR05', 'P05', 1),
+('TR04', 'P04', 2);
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `account`
+--
+ALTER TABLE `account`
+  ADD PRIMARY KEY (`accNo`);
+
+--
 -- Indexes for table `customer`
 --
 ALTER TABLE `customer`
-  ADD PRIMARY KEY (`custId`);
+  ADD PRIMARY KEY (`custNo`);
 
 --
 -- Indexes for table `product`
 --
 ALTER TABLE `product`
   ADD PRIMARY KEY (`prodCode`);
-
---
--- Indexes for table `sales`
---
-ALTER TABLE `sales`
-  ADD PRIMARY KEY (`salesNum`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `customer`
---
-ALTER TABLE `customer`
-  MODIFY `custId` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `product`
---
-ALTER TABLE `product`
-  MODIFY `prodCode` int(10) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `sales`
---
-ALTER TABLE `sales`
-  MODIFY `salesNum` int(10) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
