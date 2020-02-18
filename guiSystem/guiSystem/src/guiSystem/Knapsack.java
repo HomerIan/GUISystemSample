8226290b857c8e13d362a072a2e799faca43d0a6
HomerIan <47555113+HomerIan@users.noreply.github.com>
2020-02-18 23:22:04 +0800

    Update Knapsack.java
    3rd commit

--- guiSystem/guiSystem/src/guiSystem/Knapsack.java
+++ guiSystem/guiSystem/src/guiSystem/Knapsack.java
@@ -8,77 +8,83 @@ public class Knapsack {
 
 	public static void Knapsack(int custNum[], int weight[], int value[]) {
 		int totalweight = 0, totalcost = 0, capacity = 100;
-
+		// SINGLE DIGIT
 		for (int i = 0; i < custNum.length; i++) {
+			totalweight = weight[i];
+			totalcost = value[i];
+			if (totalweight <= capacity) {
+				remarks = "Accepted";
+			} else {
+				remarks = "Rejected";
+			}
+			System.out.println(custNum[i] + "\t" + totalweight + "\t" + totalcost + "\t" + remarks);
+		}
+		// 2 DIGITS COMBINATION
+		for (int i = 1; i < custNum.length; i++) {
 			for (int j = i + 1; j < custNum.length + 1; j++) {
-				if (i != 0) {
-					totalweight = weight[i - 1] + weight[j - 1];
-					totalcost = value[i - 1] + value[j - 1];
-					if (totalweight <= capacity) {
-						remarks = "Accepted";
-					} else {
-						remarks = "Rejected";
-					}
-					System.out.println(i + "," + j + "\t" + totalweight + "\t" + totalcost + "\t" + remarks);
+				totalweight = weight[i - 1] + weight[j - 1];
+				totalcost = value[i - 1] + value[j - 1];
+				if (totalweight <= capacity) {
+					remarks = "Accepted";
 				} else {
-					totalweight = weight[j - 1];
-					totalcost = value[j - 1];
-					if (totalweight <= capacity) {
-						remarks = "Accepted";
-					} else {
-						remarks = "Rejected";
-					}
-					System.out.println(j + "\t" + totalweight + "\t" + totalcost + "\t" + remarks);
+					remarks = "Rejected";
 				}
+				System.out.println(i + "," + j + "\t" + totalweight + "\t" + totalcost + "\t" + remarks);
 			}
 		}
-		int a = 0, b = 1, c = 1;
+		// 3 DIGITS COMBINATION
+		int num1 = 0, num2 = 1, num3 = 1, num4 = 1;
 		for (int i = 0; i < custNum.length * 2; i++) {
-			totalweight = weight[a] + weight[a + b] + weight[a + b + c];
-			totalcost = value[a] + value[a + b] + value[a + b + c];
+			totalweight = weight[num1] + weight[num1 + num2] + weight[num1 + num2 + num3];
+			totalcost = value[num1] + value[num1 + num2] + value[num1 + num2 + num3];
 			if (totalweight <= capacity) {
 				remarks = "Accepted";
 			} else {
 				remarks = "Rejected";
 			}
-			System.out.println(custNum[a] + "," + custNum[a + b] + "," + custNum[a + b + c] + "\t" + totalweight + "\t"
-					+ totalcost + "\t" + remarks);
-			++c;
-			if (a + b + c == 5) {
-				b++;
-				c = 1;
+			System.out.println(custNum[num1] + "," + custNum[num1 + num2] + "," + custNum[num1 + num2 + num3] + "\t"
+					+ totalweight + "\t" + totalcost + "\t" + remarks);
+			++num3;
+			if (num1 + num2 + num3 == 5) {
+				num2++;
+				num3 = 1;
 			}
-			if (a + b == 4) {
-				a++;
-				b = 1;
+			if (num1 + num2 == 4) {
+				num1++;
+				num2 = 1;
 			}
 		}
-		int d = 0, e = 1, f = 1, g = 1;
+		num1 = 0;// REINITIALIZE
+		num2 = 1;
+		num3 = 1;
+		// 4 DIGITS COMBINATION
 		for (int i = 0; i < custNum.length; i++) {
-			totalweight = weight[d] + weight[d + e] + weight[d + e + f] + weight[d + e + f + g];
-			totalcost = value[d] + value[d + e] + value[d + e + f] + value[d + e + f + g];
+			totalweight = weight[num1] + weight[num1 + num2] + weight[num1 + num2 + num3]
+					+ weight[num1 + num2 + num3 + num4];
+			totalcost = value[num1] + value[num1 + num2] + value[num1 + num2 + num3] + value[num1 + num2 + num3 + num4];
 			if (totalweight <= capacity) {
 				remarks = "Accepted";
 			} else {
 				remarks = "Rejected";
 			}
-			System.out.println(custNum[d] + "," + custNum[d + e] + "," + custNum[d + e + f] + ","
-					+ custNum[d + e + f + g] + "\t" + totalweight + "\t" + totalcost + "\t" + remarks);
-			++g;
-			if (d + e + f + g == 5) {
-				f++;
-				g = 1;
+			System.out.println(custNum[num1] + "," + custNum[num1 + num2] + "," + custNum[num1 + num2 + num3] + ","
+					+ custNum[num1 + num2 + num3 + num4] + "\t" + totalweight + "\t" + totalcost + "\t" + remarks);
+			++num4;
+			if (num1 + num2 + num3 + num4 == 5) {
+				num3++;
+				num4 = 1;
 			}
-			if (d + e + f == 4) {
-				e++;
-				f = 1;
+			if (num1 + num2 + num3 == 4) {
+				num2++;
+				num3 = 1;
 			}
-			if (d + e == 3) {
-				d++;
-				e = 1;
+			if (num1 + num2 == 3) {
+				num1++;
+				num2 = 1;
 			}
 
 		}
+		// 5 DIGITS COMBINATION
 		for (int i = 0; i < 1; i++) {
 			totalweight = weight[i] + weight[i + 1] + weight[i + 2] + weight[i + 3] + weight[i + 4];
 			totalcost = value[i] + value[i + 1] + value[i + 2] + value[i + 3] + value[i + 4];
@@ -119,3 +125,4 @@ public class Knapsack {
 
 	}
 }
+
